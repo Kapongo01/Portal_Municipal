42a8029bc6bacd7d49c5de9da7fb6c1587a705a8
KapongoJose <kapongo.jose@multitel.co.ao>
2025-06-29 20:49:46 +0100

    segundo commit

---
+++ src/app/Home/hero/hero.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'app-hero',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+  
+}
+
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopBarComponent } from "./Home/top-bar/top-bar.component";
+import { MenuComponent } from "./Home/menu/menu.component";
+import { HeroComponent } from "./Home/hero/hero.component";
+import { CommonModule } from '@angular/common';
+import { ServicosComponent } from "./Home/servicos/servicos.component";
+import { NoticiasComponent } from "./Home/noticias/noticias.component";
+import { CtaComponent } from "./Home/cta/cta.component";
+import { ContactoComponent } from "./Home/contacto/contacto.component";
+import { FooterComponent } from "./Home/footer/footer.component";
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, TopBarComponent, MenuComponent, HeroComponent, ServicosComponent, NoticiasComponent, CtaComponent, ContactoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
